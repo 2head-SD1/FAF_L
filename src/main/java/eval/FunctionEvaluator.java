2c3f06bc3d519535f4ba3be48f18f934ffd43fa5
Gleb Smetanin <g.o.smetanin@gmail.com>
2022-05-24 23:33:21 +0300

    Lambdas have been added.

--- src/main/java/eval/FunctionEvaluator.java
+++ src/main/java/eval/FunctionEvaluator.java
@@ -9,7 +9,8 @@ public class FunctionEvaluator
     public static boolean isFunctionExpr(Expr expr)
     {
         return expr instanceof Define ||
-                expr instanceof FuncCall;
+                expr instanceof FuncCall ||
+                expr instanceof Lambda;
     }
 
     public static Expr doFunctionExpr(Expr expr) throws Exception
@@ -24,6 +25,11 @@ public class FunctionEvaluator
             FuncCall call = (FuncCall) expr;
             return doCall(call);
         }
+        if (expr instanceof  Lambda)
+        {
+            Lambda lambda = (Lambda) expr;
+            return doLambda(lambda);
+        }
 
         throw new Exception("Cannot handle such action with functions");
     }
@@ -35,12 +41,32 @@ public class FunctionEvaluator
         return define;
     }
 
+    private static Expr doLambda(Lambda lambda) throws Exception
+    {
+        return lambda;
+    }
+
     private static Expr doCall(FuncCall call) throws Exception
     {
         SymbolTable.increaseScopeLevel();
-        Define define = (Define) SymbolTable.getSymbol(call.ident_).value;
-        addSymbols(define, call);
-        Expr toReturn = Evaluator.evalStep(define.expr_);
+        Expr exprFromTable = SymbolTable.getSymbol(call.ident_).value;
+        Expr toReturn;
+        if (exprFromTable instanceof Define)
+        {
+            Define define = (Define) exprFromTable;
+            addSymbols(define, call);
+            toReturn = Evaluator.evalStep(define.expr_);
+        }
+        else if (exprFromTable instanceof Lambda)
+        {
+            Lambda lambda = (Lambda) exprFromTable;
+            addSymbols(lambda, call);
+            toReturn = Evaluator.evalStep(lambda.expr_);
+        }
+        else
+        {
+            throw new Exception("Cannot call such type");
+        }
         SymbolTable.decreaseScopeLevel();
         return toReturn;
     }
@@ -50,6 +76,17 @@ public class FunctionEvaluator
         for (int i = 0; i < define.listatypedarg_.size(); i++)
         {
             TypedArg arg = (TypedArg) define.listatypedarg_.get(i);
+            Expr toArgExpr = Evaluator.evalStep(call.listexpr_.get(i));
+            SymbolNode node = new SymbolNode(arg.type_, toArgExpr);
+            SymbolTable.addSymbol(arg.ident_, node);
+        }
+    }
+
+    private static void addSymbols(Lambda lambda, FuncCall call) throws Exception
+    {
+        for (int i = 0; i < lambda.listatypedarg_.size(); i++)
+        {
+            TypedArg arg = (TypedArg) lambda.listatypedarg_.get(i);
             Expr toArgExpr = Evaluator.evalStep(call.listexpr_.get(i));
             SymbolNode node = new SymbolNode(arg.type_, toArgExpr);
             SymbolTable.addSymbol(arg.ident_, node);
